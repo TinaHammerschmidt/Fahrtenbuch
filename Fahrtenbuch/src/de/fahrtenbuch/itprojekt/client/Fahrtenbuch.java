8dd13a08aefa0426090e043b05e199c51e443b00
Benjamin Auwärter <ba023@HdM-Stuttgart.de>
2015-04-22 13:00:05 +0200

    Commit #2

--- Fahrtenbuch/src/de/fahrtenbuch/itprojekt/client/Fahrtenbuch.java
+++ Fahrtenbuch/src/de/fahrtenbuch/itprojekt/client/Fahrtenbuch.java
@@ -14,7 +14,7 @@ public class Fahrtenbuch implements EntryPoint {
 	 */
 	public void onModuleLoad() {
 		
-		System.out.println("Entry Test");
+		System.out.println("Entry THIS IS SPARTA");
 
 	}
 }
